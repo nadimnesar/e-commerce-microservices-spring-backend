f4feb4876f168b3a66e51b08dcc7d27f4b749a50
nadimnesar <nadimnesar.cse@gmail.com>
2024-10-26 12:40:21 +0600

    cart, order and payment service registered as client with discovery server

--- cart-service/src/main/java/com/nadimnesar/ecommerce/cart_service/CartServiceApplication.java
+++ cart-service/src/main/java/com/nadimnesar/ecommerce/cart_service/CartServiceApplication.java
@@ -2,7 +2,9 @@ package com.nadimnesar.ecommerce.cart_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CartServiceApplication {
 
